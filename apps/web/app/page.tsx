65dbc01e04f0ff01fbf43eef9a7c49e390c0a710
Rajat Gangwar <rajat.gangwar41@gmail.com>
2025-09-08 18:46:56 +0530

    03:clerk authentication

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,22 +1,38 @@
 "use client";
 
+import { SignInButton, UserButton } from "@clerk/nextjs";
 import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
-import { useQuery, useMutation } from "convex/react";
+import {
+  useQuery,
+  useMutation,
+  Authenticated,
+  Unauthenticated,
+} from "convex/react";
 
 export default function Page() {
   const users = useQuery(api.users.getUsers);
   const addUser = useMutation(api.users.addUser);
 
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello App/Web</h1>
-        <Button onClick={() => addUser({ name: "Randy Newman" as string })}>
-          Add User
-        </Button>
-        Users: {users!?.map((u) => u.name).join(", ")}
-      </div>
-    </div>
+    <>
+      <Authenticated>
+        <div className="flex items-center justify-center min-h-svh">
+          <div className="flex flex-col items-center justify-center gap-4">
+            <h1 className="text-2xl font-bold">Hello App/Web</h1>
+            <Button onClick={() => addUser({ name: "Randy Newman" as string })}>
+              Add User
+            </Button>
+            Users: {users!?.map((u) => u.name).join(", ")}
+          </div>
+          <UserButton />
+        </div>
+      </Authenticated>
+      <Unauthenticated>
+        <SignInButton>
+          <Button className="">Sign Up</Button>
+        </SignInButton>
+      </Unauthenticated>
+    </>
   );
 }
--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,16 +1,21 @@
 "use client";
 
 import { api } from "@workspace/backend/_generated/api";
-import { useQuery } from "convex/react";
+import { Button } from "@workspace/ui/components/button";
+import { useQuery, useMutation } from "convex/react";
 
 export default function Page() {
   const users = useQuery(api.users.getUsers);
+  const addUser = useMutation(api.users.addUser);
   console.log("🚀 ~ Page ~ users:", users);
 
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello App/Widget</h1>
+        <Button onClick={() => addUser({ name: "Randy Newman" as string })}>
+          Add User
+        </Button>
         Users: {users!?.map((u) => u.name).join(", ")}
       </div>
     </div>
