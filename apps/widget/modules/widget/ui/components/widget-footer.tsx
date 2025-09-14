6ba6ec90b45f9740d6ff9bcb918c61297636d2be
Rajat Gangwar <rajat.gangwar41@gmail.com>
2025-09-14 19:39:34 +0530

    16: widget inbox

--- apps/widget/modules/widget/ui/components/widget-footer.tsx
+++ apps/widget/modules/widget/ui/components/widget-footer.tsx
@@ -1,15 +1,19 @@
 import { Button } from "@workspace/ui/components/button";
 import { cn } from "@workspace/ui/lib/utils";
+import { useAtom } from "jotai";
 import { HomeIcon, InboxIcon } from "lucide-react";
+import { screenAtom } from "../../atoms/widget-atoms";
 
 export const WidgetFooter = () => {
-  const screen = "selection";
+  const [screen, setScreen] = useAtom(screenAtom);
 
   return (
     <footer className="flex items-center justify-between border-t bg-background">
       <Button
         className="h-14 flex-1 rounded-none"
-        onClick={() => {}}
+        onClick={() => {
+          setScreen("selection");
+        }}
         size="icon"
         variant="ghost"
       >
@@ -19,12 +23,14 @@ export const WidgetFooter = () => {
       </Button>
       <Button
         className="h-14 flex-1 rounded-none"
-        onClick={() => {}}
+        onClick={() => {
+          setScreen("inbox");
+        }}
         size="icon"
         variant="ghost"
       >
         <InboxIcon
-          className={cn("size-5", screen === "selection" && "text-primary")}
+          className={cn("size-5", screen === "inbox" && "text-primary")}
         />
       </Button>
     </footer>
