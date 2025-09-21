31e014484d6c9471f9a78d31b702964030f3b1a2
Rajat Gangwar <rajat.gangwar41@gmail.com>
2025-09-21 14:42:49 +0530

    30: contact panel

--- apps/web/app/(dashboard)/files/page.tsx
+++ apps/web/app/(dashboard)/files/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import FilesView from "@/modules/files/ui/views/files-view";
 
-const Customization = () => {
+const Files = () => {
   return <FilesView />;
 };
 
-export default Customization;
+export default Files;
