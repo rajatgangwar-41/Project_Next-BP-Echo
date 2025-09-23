fd60ddcd0354b981c2e69872ce74c794d878cc9a
Rajat Gangwar <rajat.gangwar41@gmail.com>
2025-09-23 15:10:28 +0530

    35: deployment

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,8 +1,17 @@
-import {withSentryConfig} from "@sentry/nextjs";
+import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
-}
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/conversations",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default withSentryConfig(nextConfig, {
   // For all available options, see:
